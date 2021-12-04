87add88ba16b5688d819adc69d06679f0a4791ee
Karol <95040316+KarolHenriques@users.noreply.github.com>
2021-12-04 20:14:46 +0000

    Alterações JS
    Agora, uma pequena explicação sobre as bases convertidas aparece em uma caixa apenas quando alguma conta é feita. Por favor, testem e digam o que pensam.

--- scripts.js
+++ scripts.js
@@ -5,6 +5,7 @@
 */
 function convert() {
 
+    //Conversão Decimal para Binário:
     if (flexRadioDefaultdec.checked && flexRadioDefaultbin1.checked){
     // Aqui, é feita a conversão de decimal para binário. toString retorna a string de um número
     //Nesta caso, "base1", que é o valor entrada no HTML corresponde ao número decimal.
@@ -14,9 +15,11 @@ function convert() {
     // converter para binário:
     const result = number.toString(2);
 
-    document.getElementById("Converter").innerHTML=("Aqui:" + ' ' + 'Binary:' + ' ' + result);
-}
+    document.getElementById("Converter").innerHTML=("Binário:" + ' ' + result + "<p></p>" + "<h4>Mais sobre as bases<h4>" + "<p></p>" + "<h5>Sistema Decimal:</h5>" + "<p></p>" + "O princípio fundamental do sistema decimal é que dez unidades de uma ordem qualquer formam uma de ordem imediatamente superior. Depois das ordens, as unidades constitutivas dos números são agrupadas em classes, em que cada classe tem três ordens, em que cada ordem tem uma denominação especial sendo idênticas às mesmas ordens de outras classes." + "<p></p>" + "<h5>Sistema Binário:</h5>" + "O sistema binário ou de base 2 é um sistema de numeração posicional em que todas asquantidades se representam com base em dois números, ou seja, zero e um (0 e 1)." );
+    
+    }
 
+    //Conversão Binário para Decimal:
     if (flexRadioDefaultbin.checked && flexRadioDefaultdec1.checked){
     //Aqui, é feita a conversão de binário para decimal.
     //Nesta caso, "base1", que é o valor entrada no HTML corresponde ao número binário.
@@ -26,12 +29,18 @@ function convert() {
     // converter para decimal:
     const result = parseInt(number, 2);
 
-    document.getElementById("Converter").innerHTML=("Aqui:" + 'Decimal:' + ' ' + result);
-}
+    document.getElementById("Converter").innerHTML=("Decimal:" + ' ' + result + "<p></p>" + "<h4>Mais sobre as bases<h4>" + "<h5>Sistema Binário:</h5>" + "O sistema binário ou de base 2 é um sistema de numeração posicional em que todas asquantidades se representam com base em dois números, ou seja, zero e um (0 e 1)." + "<p></p>" + "<h5>Sistema Decimal:</h5>" + "<p></p>" + "O princípio fundamental do sistema decimal é que dez unidades de uma ordem qualquer formam uma de ordem imediatamente superior. Depois das ordens, as unidades constitutivas dos números são agrupadas em classes, em que cada classe tem três ordens, em que cada ordem tem uma denominação especial sendo idênticas às mesmas ordens de outras classes."  );
+    }
 
-if (flexRadioDefaultbin.checked && flexRadioDefaultbin1.checked || flexRadioDefaultdec.checked && flexRadioDefaultdec1.checked || flexRadioDefaultoct.checked && flexRadioDefaultoct1.checked || flexRadioDefaulthex.checked && flexRadioDefaulthex1.checked ){
+
+//Alerta para tentar converter um número para a mesma base em que o mesmo se encontra    
+    if (flexRadioDefaultbin.checked && flexRadioDefaultbin1.checked || flexRadioDefaultdec.checked && flexRadioDefaultdec1.checked || flexRadioDefaultoct.checked && flexRadioDefaultoct1.checked || flexRadioDefaulthex.checked && flexRadioDefaulthex1.checked ){
     alert("Os números devem estar em bases diferentes para realizar a conversão");
-}
+    }
+
+    else {
+        document.getElementById('Converter').style.visibility='visible' ;
+  }
 
 
 
