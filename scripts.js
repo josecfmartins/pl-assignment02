0f12ca382f3d06a4ccc6b98995c3e8b93e24602a
tiagoalmeida100 <95497339+tiagoalmeida100@users.noreply.github.com>
2021-12-05 20:18:17 +0000

    Conversão de Binário para Hexadecimal e vice-versa && Binário para Octagonal e vice-versa

--- scripts.js
+++ scripts.js
@@ -85,6 +85,67 @@ function convert() {
     
         document.getElementById("Converter").innerHTML=("Decimal:" + ' ' + result + "<p></p>" + "<h4>Mais sobre as bases<h4>" + "<h5>Sistema Hexadecimal:</h5>" + "O sistema hexadecimal é um sistema de numeração posicional que representa os números em base 16, empregando, portanto, 16 símbolos" + "<p></p>" + "Conjunto de simbolos: S = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, E, F}" + "<h5>Sistema Decimal:</h5>" + "<p></p>" + "O princípio fundamental do sistema decimal é que dez unidades de uma ordem qualquer formam uma de ordem imediatamente superior. Depois das ordens, as unidades constitutivas dos números são agrupadas em classes, em que cada classe tem três ordens, em que cada ordem tem uma denominação especial sendo idênticas às mesmas ordens de outras classes."  );
         }
+    
+    
+    
+    
+    
+    //Conversão Binário para Hexadecimal:
+        if (flexRadioDefaultbin.checked && flexRadioDefaulthex1.checked){
+        // Aqui, é feita a conversão de Binário para Hexadecimal. toString retorna a string de um número
+        //Nesta caso, "base1", que é o valor entrada no HTML corresponde ao número decimal.
+        // O método toString [8] retorna a string na base 8; ou seja, retorna o número binário daquele decimal.
+        const number = parseInt(document.getElementById("base1").value);
+    
+        // converter para Hexadecimal:
+        const result = number.toString(6);
+    
+        document.getElementById("Converter").innerHTML=("Hexadecimal:" + ' ' + result + "<p></p>" + "<h4>Mais sobre as bases<h4>" + "<p></p>" + "<h5>Sistema Binário:</h5>" + "<p></p>" + "O sistema binário ou de base 2 é um sistema de numeração posicional em que todas asquantidades se representam com base em dois números, ou seja, zero e um (0 e 1)." + "<p></p>" + "<h5>Sistema Hexadecimal:</h5>" + "<p></p>" + "O sistema hexadecimal é um sistema de numeração posicional que representa os números em base 16, empregando, portanto, 16 símbolos" + "<p></p>" + "Conjunto de simbolos: S = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, E, F}"  );
+        
+        }
+    
+        //Conversão Hexadecimal para Binário:
+        if (flexRadioDefaulthex.checked && flexRadioDefaultbin1.checked){
+        //Aqui, é feita a conversão de Hexadecimal para Binário.
+        //Nesta caso, "base1", que é o valor entrada no HTML corresponde ao número binário.
+        const number = parseInt(document.getElementById("base1").value);
+    
+        // converter para Binário:
+        const result = parseInt(number, 6);
+    
+        document.getElementById("Converter").innerHTML=("Binário:" + ' ' + result + "<p></p>" + "<h4>Mais sobre as bases<h4>" + "<h5>Sistema Hexadecimal:</h5>" + "O sistema hexadecimal é um sistema de numeração posicional que representa os números em base 16, empregando, portanto, 16 símbolos" + "<p></p>" + "Conjunto de simbolos: S = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, E, F}" + "<p></p>" + "<h5>Sistema Binário:</h5>" + "<p></p>" + "O sistema binário ou de base 2 é um sistema de numeração posicional em que todas as quantidades se representam com base em dois números, ou seja, zero e um (0 e 1)." );
+        }
+  
+        
+       //Conversão Binário para Octal:
+       if (flexRadioDefaultbin.checked && flexRadioDefaultoct1.checked){
+        // Aqui, é feita a conversão de binário para octal. toString retorna a string de um número
+        //Nesta caso, "base1", que é o valor entrada no HTML corresponde ao número binário.
+        // O método toString [8] retorna a string na base 8; ou seja, retorna o número binário daquele octal.
+        const number = parseInt(document.getElementById("base1").value);
+    
+        // converter para octal:
+        const result = number.toString(8);
+    
+        document.getElementById("Converter").innerHTML=("Octagonal:" + ' ' + result + "<p></p>" + "<h4>Mais sobre as bases<h4>" + "<p></p>" + "<h5>Sistema Binário:</h5>" + "<p></p>" + "O sistema binário ou de base 2 é um sistema de numeração posicional em que todas as quantidades se representam com base em dois números, ou seja, zero e um (0 e 1)." + "<p></p>" + "<h5>Sistema Octagonal:</h5>" + "Sistema Octagonal é um sistema de numeração cuja base é 8, ou seja, utiliza 8 símbolos para a representação de quantidade." + "<p></p>" + "No ocidente, estes símbolos são os algarismos arábicos. O octal foi muito utilizado em informática como uma alternativa ais compacta ao sistema binário na programação" );
+        
+        }
+       
+        //Conversão Octal para Binário:
+    if (flexRadioDefaultoct.checked && flexRadioDefaultbin1.checked){
+        // Aqui, é feita a conversão de octal para binário. toString retorna a string de um número
+        //Nesta caso, "base1", que é o valor entrada no HTML corresponde ao número octal.
+        // O método toString [2] retorna a string na base 2; ou seja, retorna o número binário daquele octal.
+        const number = parseInt(document.getElementById("base1").value);
+    
+        // converter para binário:
+        const result = number.toString(2);
+    
+        document.getElementById("Converter").innerHTML=("Binário:" + ' ' + result + "<p></p>" + "<h4>Mais sobre as bases<h4>" + "<p></p>" + "<h5>Sistema Octagonal:</h5>" + "<p></p>" + "Sistema Octagonal é um sistema de numeração cuja base é 8, ou seja, utiliza 8 símbolos para a representação de quantidade." + "<p></p>" + "No ocidente, estes símbolos são os algarismos arábicos. O octal foi muito utilizado em informática como uma alternativa ais compacta ao sistema binário na programação" + "<p></p>" + "<h5>Sistema Binário:</h5>" + "O sistema binário ou de base 2 é um sistema de numeração posicional em que todas asquantidades se representam com base em dois números, ou seja, zero e um (0 e 1)." );
+        
+        }
+    
+    
 
 
 //Alerta para tentar converter um número para a mesma base em que o mesmo se encontra    
